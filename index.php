04f21e1a2bdb78a1a319c7a6155a772bad1bf47b
Roman Gsponer <mail@microman.ch>
2025-11-10 07:56:06 +0100

    Compatibility Kirby 5.1.3

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Kirby\Cms\App;
 //Cause modification in Commit b0b92b6 (https://github.com/getkirby/kirby//commit/b0b92b6985f4e848f63a40904e1d346190dc5e7d)
 
 if ( version_compare(App::version() ?? '0.0.0', '5.1.3', '>') === true ) {
-    throw new Exception('Kirby Toggles Suite requires Kirby 5.1.3 or higher.');
+    throw new Exception('Update Kirby to 5.1.3 or downgrade Kirby Toggles Suite to 1.0.5.');
 }
 
 @include_once __DIR__ . "/utils/load.php";
